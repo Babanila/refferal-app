8eb403e81dccd8dbd0182d64279acfba526c1c9b
babanila <babanila@yahoo.com>
2020-03-14 23:55:50 +0100

    feat(): modify user properties

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -4,29 +4,62 @@ async function feed(parent, args, context) {
       where: {
         OR: [
           { description_contains: args.filter },
-          { url_contains: args.filter },
-        ],
-      },
+          { url_contains: args.filter }
+        ]
+      }
     })
     .aggregate()
-    .count()
+    .count();
+
   const links = await context.prisma.links({
     where: {
-      OR: [
-        { description_contains: args.filter },
-        { url_contains: args.filter },
-      ],
+      OR: [{ description_contains: args.filter }, { url_contains: args.filter }]
     },
     skip: args.skip,
     first: args.first,
-    orderBy: args.orderBy,
-  })
+    orderBy: args.orderBy
+  });
+
   return {
     count,
-    links,
-  }
+    links
+  };
+}
+
+async function user(parent, args, context) {
+  const count = await context.prisma
+    .usersConnection({
+      where: {
+        OR: [
+          { firstname_contains: args.filter },
+          { lastname_contains: args.filter },
+          { email_contains: args.filter }
+        ]
+      }
+    })
+    .aggregate()
+    .count();
+
+  const users = await context.prisma.users({
+    where: {
+      OR: [
+        { firstname_contains: args.filter },
+        { lastname_contains: args.filter },
+        { email_contains: args.filter }
+      ]
+    },
+    skip: args.skip,
+    first: args.first,
+    orderBy: args.orderBy
+  });
+
+  return {
+    count,
+    users
+  };
 }
 
 module.exports = {
   feed,
-}
+  user
+};
---
+++ src/components/Link.js
@@ -0,0 +1,15 @@
+import { jsx, css } from "@emotion/core";
+import React, { Component } from "react";
+
+function Link(props) {
+  const { link } = props;
+  return (
+    <div>
+      <div>
+        {link.description} ({link.url})
+      </div>
+    </div>
+  );
+}
+
+export default Link;
